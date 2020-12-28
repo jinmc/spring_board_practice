3fcfcb88f5212586799ba4deb1bc3b815a7d075c
jinmc <jinmo55555@gmail.com>
2020-12-28 10:24:11 +0900

    add spring boot web application from starter pack

---
+++ src/main/java/com/myBoard/demo/MyboardApplication.java
@@ -0,0 +1,13 @@
+package com.myBoard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myBoard/demo/MyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myBoard.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
